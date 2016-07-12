3d4ba1aa8b446284b71e6dd67fc66c4e87c01d63
lufeirider <3020207744@qq.com>
2016-07-12 17:20:19 +0800

    Update zoomeye-crawl.py

--- crawl/zoomeye-crawl.py
+++ crawl/zoomeye-crawl.py
@@ -8,6 +8,9 @@ import StringIO
 import urllib
 import certifi
 import time
+import random
+import time
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -23,8 +26,10 @@ def initCurl():
     return c
 
 def GetDate(curl, url):
+    r=random.randint(0,11)
+    
     head = ["User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
-            "Cookie: __jsluid=f608f2b6ab98ed247f42c68a9e05ebf1; csrftoken=bFL4Gzx7m2SZE3OzCo83gPELcHJQyZYU; sessionid=i0x8r978nw1l1s8k8m68d19urdr8thb0; __jsl_clearance=1468304384.174|0|8yxLmZYzVcVY1KIxa0cClTLi%2BWY%3D; Hm_lvt_e58da53564b1ec3fb2539178e6db042e=1467966546,1468215754,1468248863,1468304365; Hm_lpvt_e58da53564b1ec3fb2539178e6db042e=1468304758"]
+            "Cookie: __jsluid=f608f2b6ab98ed247f42c68a9e05ebf1; csrftoken=bFL4Gzx7m2SZE3OzCo83gPELcHJQyZYU; sessionid=i0x8r978nw1l1s8k8m68d19urdr8thb0; __jsl_clearance=1468313264.982|0|OlZxLoM%2BSfoJ0paeGsszQFz3JGg%3D; Hm_lvt_e58da53564b1ec3fb2539178e6db042e=1467966546,1468215754,1468248863,1468304365; Hm_lpvt_e58da53564b1ec3fb2539178e6db042e=1468313270"]
     buf = StringIO.StringIO()
     curl.setopt(pycurl.WRITEFUNCTION, buf.write)
     curl.setopt(pycurl.CAINFO, certifi.where())
@@ -33,6 +38,7 @@ def GetDate(curl, url):
     curl.perform()
     the_page =buf.getvalue()
     buf.close()
+    curl.close()
     return the_page
 
 def wx(filename,context):
@@ -43,9 +49,11 @@ def wx(filename,context):
 
 pn = 1
 flag = 1
-c = initCurl()
 
-while(flag):
+
+while(flag and pn!=20):
+    print "pn              {0}".format(pn)
+    c = initCurl()
     html = GetDate(c, ("https://www.zoomeye.org/search?q=APMServ%205.2.6%20country%3AChina%20port:80&p={0}&t=host").format(pn))
     pn = pn + 1
     
@@ -65,4 +73,4 @@ while(flag):
     for a in As:
         wx("zoomeye.txt", a.string+"\n")
         print a.string
-    time.sleep(2)
+    time.sleep(random.uniform(1,3))
